0c0b21d389325b6cb6c4942138d798ab47c538f9
Ricardo M. Conde Teret <ricardoconde96@gmail.com>
2022-12-19 21:37:17 -0600

    Se agrego controlador de Indicadores y las vistas respectivas

---
+++ app-ingenieria-ufinet/Controllers/IndicadoresController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace app_ingenieria_ufinet.Controllers
+{
+    public class IndicadoresController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult TablaDeDatos()
+        {
+            return View();
+        }
+    }
+}
