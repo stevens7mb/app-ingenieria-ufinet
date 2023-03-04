6c6e8742335097423864bc3d90634554ecd6e4f8
Steven Martín <steven10martin@gmail.com>
2023-03-04 13:29:28 -0600

    Orden de paginado DESC

--- app-ingenieria-ufinet/Models/Commons/DataTablePaginate/DataTablePaginateRequest.cs
+++ app-ingenieria-ufinet/Models/Commons/DataTablePaginate/DataTablePaginateRequest.cs
@@ -6,6 +6,6 @@
         public int PageNo { get; set; } = 1;
         public int PageSize { get; set; } = 10;
         public int SortColumn { get; set; }
-        public string SortDirection { get; set; } = "ASC";
+        public string SortDirection { get; set; } = "DESC";
     }
 }
